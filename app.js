1b22b6f646697bb2fb5c738e6f2bb45deeb29b53
Code-Lyogan <logantrout1@hotmail.com>
2022-10-26 09:49:29 -0400

    second commt, added readme.md

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-// Create variable called tasks. 
+// First, we create a variable called tasks. 
 // If there are no objects in our local storage, it returns an empty array and assigns it to our tasks.
 
 window.addEventListener("load", () => {
     tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    //JSON.parse will turn our tasks inside of our local storage into a string object.
+
     const nameInput = document.querySelector("#name");
    
     const newTaskForm = document.querySelector("#new-task-form"); 
-  // Creating a variable called taskInputs so we can store any inputs that come from this form into our array of tasks.
+
     const username = localStorage.getItem("username") || "";
+// variable created to gather the username of app operator, logs as key value.
+
     nameInput.value = username;
+    // sets value of username = nameInput to log into local storage
   
     nameInput.addEventListener("change", (e) => {
       localStorage.setItem("username", e.target.value);
-    });
+    }); // Event Listener fires when change is made to username using event.target
+
   // Adds a listener event, that fires when user enters their item through submit. 
     newTaskForm.addEventListener("submit", (e) => {
       e.preventDefault();
@@ -28,7 +34,10 @@ window.addEventListener("load", () => {
       tasks.push(task);
   
       localStorage.setItem("tasks", JSON.stringify(tasks));
+      // turning "tasks" inside of our JSON data into a string
+
       e.target.reset();
+
   // Displays our tasks and will update determined through user change.
       DisplayTasks();
     });
@@ -36,13 +45,17 @@ window.addEventListener("load", () => {
     DisplayTasks();
   });
   
+  //Below we make our function to display/create our tasks
+  // and remove them as we choose.
   function DisplayTasks() {
     const taskList = document.querySelector("#task-list");
     taskList.innerHTML = "";
+    //variable selecting our task-list class and displaying the inner HTML as empty string.
   
     tasks.forEach((task) => {
       const taskItem = document.createElement("div");
       taskItem.classList.add("task-item");
+      // creates new task element whenever submitted through our input.
   
       const label = document.createElement("label");
       const input = document.createElement("input");
@@ -51,36 +64,49 @@ window.addEventListener("load", () => {
       const actions = document.createElement("div");
       const edit = document.createElement("button");
       const deleteButton = document.createElement("button");
+      // creates all variable attributes of task element when added through our input.
 
-  // Making check button and done button.
+  // Making Urgent button and Later button.
 
       input.type = "checkbox";
+
       input.checked = task.done;
+
       span.classList.add("bubble");
       if (task.category == "personal") {
         span.classList.add("personal");
       } else {
         span.classList.add("business");
-      }
+      } // creates for loop, adding correct time frame bubble depending on selection.
+
+
       // Allows full edit abilities using innerHTML. 
       content.classList.add("task-content");
       actions.classList.add("actions");
       edit.classList.add("edit");
       deleteButton.classList.add("delete");
 
+      // Here we changing our innerHTML to display the task that was written after submitted.
       content.innerHTML = `<input type="text" value="${task.content}" readonly>`;
+
       edit.innerHTML = "Edit";
+
       deleteButton.innerHTML = "Delete";
   
       label.appendChild(input);
       label.appendChild(span);
+
       actions.appendChild(edit);
       actions.appendChild(deleteButton);
+
       taskItem.appendChild(label);
       taskItem.appendChild(content);
       taskItem.appendChild(actions);
   
       taskList.appendChild(taskItem);
+
+      //Append child is used to add these elements to the content within our task list.
+
   // Adds tasks to local storage for display
       if (task.done) {
         taskItem.classList.add("done");
@@ -100,11 +126,14 @@ window.addEventListener("load", () => {
         DisplayTasks();
       });
 
-  // Adds click listener to delete button 
+  // Adds an event listener to our edit and delete button to make them functional
       edit.addEventListener("click", (e) => {
         const input = content.querySelector("input");
         input.removeAttribute("readonly");
         input.focus();
+         // if edit button is clicked, it removes the readonly method
+         // allowing for change to be made to the task.
+
         input.addEventListener("blur", (e) => {
           input.setAttribute("readonly", true);
           task.content = e.target.value;
@@ -113,6 +142,8 @@ window.addEventListener("load", () => {
         });
       });
   
+      // Adds an event listener to make our delete button functional.
+      // if t parameter is not equal to our task variable, remove item from our task list.
       deleteButton.addEventListener("click", (e) => {
         tasks = tasks.filter((t) => t != task);
         localStorage.setItem("tasks", JSON.stringify(tasks));
---
+++ readme.md
@@ -0,0 +1,9 @@
+READ-ME
+
+This project was especially difficult for me in the terms of understanding the JSON & .FS. Ultimately I was unable to understand .FS or use it within the project. I spent most of the time trying to understand the JavaScript and fully understand it when implemented. I used the help of a few online resources such as W3Schools & referenced a few videos online as well to help me develop the CSS.
+
+The most fun I had during this project was the fact that I was creating a working/running application for my first. I consider it a small step to what else is yet to come in my creation process through tech.
+
+My biggest points I'd like to improve in are:
+1. JavaScript understanding
+2. Use of working with Json & .FS, as well as databases.
